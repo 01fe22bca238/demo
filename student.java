c4045e110ad26e06727e69f1a5f4317c15d9f4b6
Sanju <01fe22bca238@kletech.ac.in>
2024-02-20 14:26:42 +0530

    add

--- student.java
+++ student.java
@@ -1,11 +1,12 @@
-class student  {
+class student extends course {
     String name = "Sanju";
     String program = "BCA";
     String department = "BCA";
     public String course[] = { "C#", "Applied statistics", "JAVA" };
     public int marks[] = { 70, 100, 30 };
+
     public void Display() {
-     
+
         System.out.println("Name of the student" + name);
         System.out.println("Program" + program);
         System.out.println("Department" + department);
@@ -17,6 +18,6 @@ class student  {
     public static void main(String[] args) {
         student s = new student();
         s.Display();
-        // s.read();
+        s.read();
     }
 }
